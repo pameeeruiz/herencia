84ac7abd42001b4018990dd6758c0a66998e3161
Cisco-04 <Cisco-04@CISCO-04>
2026-02-24 08:16:04 -0600

    Agregar archivos de proyecto.

---
+++ herencia/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace herencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {object perro = new Perro();
+            object gato = new Gato();
+            object pajaro = new Pajaro();
+            ((Perro)perro).perr();
+            ((Gato)gato).gat();
+            ((Pajaro)pajaro).paj();
+            Console.ReadKey();
+        }
+        public class Animal
+        {
+            public string nacimiento { get; set; }
+            public string habitat { get; set; }
+            public int edad { get; set; }
+        }
+        public class Perro : Animal
+        {
+            public string raza { get; set; }
+
+            public Perro()
+            {
+                nacimiento = "viviparo";
+                habitat = "terrestre";
+                edad = 3;
+                raza = "Bulldog";
+            }
+
+            public void perr()
+            {
+                Console.WriteLine($"Soy un perro, mi nacimiento es {nacimiento}, mi habitat es {habitat}, mi edad es {edad} y mi raza es {raza} .");
+                
+            }
+        }
+        public class Gato : Animal
+        {
+            public int vida { get; set; }
+
+           public Gato()
+           {
+               nacimiento = "viviparo";
+               habitat = "terrestre";
+               edad = 2;
+               vida = 9;
+           }
+            public void gat()
+            {
+                Console.WriteLine($"Soy un gato, mi nacimiento es {nacimiento}, mi habitat es {habitat}, mi edad es {edad} y estoy en mi {vida} vida.");
+            }
+        }
+
+        public class Pajaro : Animal
+        {
+            public string color { get; set; }
+            public Pajaro()
+            {
+                nacimiento = "oviparo";
+                habitat = "aereo";
+                edad = 1;
+                color = "rojo";
+            }
+            public void paj()
+            {
+                Console.WriteLine($"Soy un pajaro, mi nacimiento es {nacimiento}, mi habitat es {habitat}, mi edad es {edad} y mi color es {color}.");
+            }
+        }
+    }
+}
